00c127775fdd1ab7597c1ccc4a768ac8f34d9a26
tobiasgatti02 <tobiasgatti02@gmail.com>
2024-04-09 16:30:42 -0300

    comment

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const axios = require('axios');
-
+//axios para comunicar con mockapi
 const PORT = process.env.PORT || 3001;
 
 
